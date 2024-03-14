7ad0e603f0bde40d8a4004812a3d94cd4a34f67d
Igor <igrik82@gmail.com>
2024-03-14 19:34:37 +0500

    feat: added time compiling

--- 00_hello_world/compile.sh
+++ 00_hello_world/compile.sh
@@ -2,12 +2,18 @@
 INFILENAME=$1
 OUTFILENAME=$2
 
-if [ -z $INFILENAME ]; then
+if [ -z ${INFILENAME} ]; then
     INFILENAME="main.cpp"
 fi
 
-if [ -z $OUTFILENAME ]; then
+if [ -z ${OUTFILENAME} ]; then
     OUTFILENAME="main"
 fi
 
-clang++ --debug $INFILENAME -O0 -gdwarf-2 -std=c++17 -Wall -o $OUTFILENAME
+ORANGE='\033[0;33m'
+WHITE='\033[1;37m'
+NC='\033[0m'
+
+printf "${ORANGE}Compiling \"${INFILENAME}\" to \"${OUTFILENAME}\"...${NC}\n"
+printf "${WHITE}Time elapsed:"
+time $(clang++ --debug ${INFILENAME} -O0 -gdwarf-2 -std=c++17 -Wall -o ${OUTFILENAME})
