491939ac3bb93714b54da29b90c1ba57d5b2247b
Igor <igrik82@gmail.com>
2024-03-14 16:48:46 +0500

    first programm

---
+++ 00_hello_world/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Input your favorite number: ";
+  int number;
+  std::cin >> number;
+  std::cout << "Your favorite number is: " << number << std::endl;
+  return 0;
+}
