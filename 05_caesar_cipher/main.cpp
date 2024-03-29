e1e62c2c6b3ac2b3be41c152965a103e4f69b781
Igor <igrik82@gmail.com>
2024-03-29 18:34:33 +0500

    It's kinda works...

--- 05_caesar_cipher/main.cpp
+++ 05_caesar_cipher/main.cpp
@@ -1,4 +1,6 @@
+#include <cstdlib>
 #include <iostream>
+#include <limits>
 #include <string>
 
 using std::cin;
@@ -6,43 +8,89 @@ using std::cout;
 using std::endl;
 using std::string;
 
-u_int8_t get_key(string key_phrase);
+int get_key(string key_phrase);
 string encrypt(string message, u_int8_t key);
 string decrypt(string message, u_int8_t key);
+void buffer_clear();
 
-const string alphabet =
-    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+const string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int action{0};
 
 int main() {
-  cout << "Ceaser Cipher" << endl;
-  cout << "====================" << endl;
-  cout << "1. Encrypt a message" << endl;
-  cout << "2. Decrypt a message" << endl;
-  cout << "3. Exit\n" << endl;
-  cout << "Enter your action: ";
-  int action;
-  cin >> action;
+  while (action != 3) {
+    // Clear cin buffer
+    if (action != 0)
+      buffer_clear();
+    system("clear");
 
-  switch (action) {
-  case 1: {
+    cout << "\nCeaser Cipher" << endl;
+    cout << "====================" << endl;
+    cout << "1. Encrypt a message" << endl;
+    cout << "2. Decrypt a message" << endl;
+    cout << "3. Exit\n" << endl;
+    cout << "Enter your action: ";
+    cin >> action;
 
-  } break;
+    switch (action) {
+    case 1: {
+      // Clear cin buffer
+      buffer_clear();
+      cout << "Enter your message: ";
+      string message{};
+      std::getline(cin, message);
+      if (message.empty()) {
+        cout << "The message can't be empty" << endl;
+        continue;
+      }
 
-  case 2: {
-  } break;
+      cout << "Enter your key phrase: ";
+      string key_phrase{};
+      std::getline(cin, key_phrase);
+      u_int8_t key = get_key(key_phrase);
 
-  case 3: {
-  } break;
+      cout << "==========================" << endl;
+      string enc_message{};
+      enc_message = encrypt(message, key);
+      cout << "Encrypted message - " << enc_message << endl;
+      string dec_message{};
+      cout << "Decrypted message - " << decrypt(enc_message, key) << endl;
+      cout << "==========================" << endl;
+    } break;
 
-  default:
-    cout << "Invalid action" << endl;
-    break;
+    case 2: {
+      // Clear cin buffer
+      buffer_clear();
+      cout << "Enter your encrypted message: ";
+      string message{};
+      std::getline(cin, message);
+      if (message.empty()) {
+        cout << "The message can't be empty" << endl;
+        continue;
+      }
+
+      cout << "Enter your key phrase: ";
+      string key_phrase{};
+      std::getline(cin, key_phrase);
+      u_int8_t key = get_key(key_phrase);
+      string dec_message{};
+      cout << "==========================" << endl;
+      cout << "Decripted message - " << decrypt(message, key) << endl;
+      cout << "==========================" << endl;
+    } break;
+
+    case 3: {
+    }
+      continue;
+
+    default:
+      cout << "Invalid action" << endl;
+      buffer_clear();
+      continue;
+    }
   }
-
-  return 0;
 }
 
-u_int8_t get_key(string key_phrase) {
+int get_key(string key_phrase) {
 
   // The phrase can't be empty
   if (key_phrase == "") {
@@ -51,17 +99,37 @@ u_int8_t get_key(string key_phrase) {
   }
 
   int temp{};
-  u_int8_t key{};
+  int key{};
 
   for (int i : key_phrase) {
     temp += i;
   }
 
-  // Return value not exided 62
+  // Return value not exided alphabet
   // and not 0
-  key = temp % 62;
+  key = temp % (alphabet.length() - 1);
   if (!key)
     key = 1;
 
   return key;
+}
+
+void buffer_clear() {
+  cin.clear();
+  cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+string encrypt(string message, u_int8_t key) {
+  string encripted{};
+  for (char index : message) {
+    encripted += alphabet.at((alphabet.find(index) + key) % alphabet.length());
+  }
+  return encripted;
+}
+string decrypt(string message, u_int8_t key) {
+  string decrypted{};
+  for (char index : message) {
+    decrypted += alphabet.at((alphabet.find(index) - key) % alphabet.length());
+  }
+  return decrypted;
 }
