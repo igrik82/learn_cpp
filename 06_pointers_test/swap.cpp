03e068feb20d47d843b3c2fbdada8d72aa92bf9d
Igor <igrik82@gmail.com>
2024-04-02 11:33:24 +0500

    Simle swap function

--- 06_pointers_test/swap.cpp
+++ 06_pointers_test/swap.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void swap(int *a, int *b);
+void swapping(int *a, int *b);
 
 int main() {
   int var_a = 10;
@@ -11,17 +11,20 @@ int main() {
   cout << "Variable var_a: " << var_a << "\n";
   cout << "Variable var_b: " << var_b << "\n\n";
 
-  swap(var_a, var_b);
+  swapping(&var_a, &var_b);
 
   cout << "After swapping:\n";
   cout << "Variable var_a: " << var_a << "\n";
   cout << "Variable var_b: " << var_b << endl;
 }
 
-void swap(int *a, int *b) {
+void swapping(int *a, int *b) {
+  // create null temporary pointer
   int *temp_ptr = new int;
-
-  temp_ptr = a;
-  a = b;
-  b = temp_ptr;
+  // store address of a in temp_ptr
+  *temp_ptr = *a;
+  // because variable can't  store the address of another variable,
+  // we store value
+  *a = *b;
+  *b = *temp_ptr;
 }
